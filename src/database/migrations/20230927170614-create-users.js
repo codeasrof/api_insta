c51fc84e5404215246e57aa0a9f18a58030b72c3
codeasrof <vitalidade.08-devorador@icloud.com>
2023-09-27 14:19:34 -0300

    Create migration of users and configs of database

---
+++ src/database/migrations/20230927170614-create-users.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id:{
+        allowNull:false,
+        autoIncrement:true,
+        primaryKey:true,
+        type: Sequelize.INTEGER,
+      },
+      name:{
+        allowNull:false,
+        type:Sequelize.STRING,
+      },
+      user_name:{
+        allowNull:false,
+        type:Sequelize.STRING,
+        unique:true
+      },
+      email:{
+        allowNull:false,
+        type: Sequelize.STRING,
+        unique:true
+      },
+      avatar:{
+        type: Sequelize.STRING
+      },
+      bio:{
+        type: Sequelize.STRING,
+      },
+      gender:{
+        type: Sequelize.STRING,
+      },
+      password_hash:{
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      created_at:{
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      updated_at:{
+        type: Sequelize.DATE,
+        allowNull:false
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+      await queryInterface.dropTable("Users")
+  }
+};
