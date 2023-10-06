fadd1527cc9481c2b9fc4722980bae74f77f4c41
codeasrof <vitalidade.08-devorador@icloud.com>
2023-10-05 22:50:15 -0300

    adding jwt

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,14 @@
 const Router = require("express");
 const schemaValidator = require("../apps/middlewares/schemaValidator");
 const UserController = require("../apps/controllers/UserController");
-const UserSchema = require("../schema/create.user.schema.json")
+const AuthenticationController = require("../apps/controllers/AuthenticationController");
+const UserSchema = require("../schema/create.user.schema.json");
 const routes = Router();
 
 
-routes.post("/users", schemaValidator(UserSchema), UserController.create)  
+routes.post("/users", schemaValidator(UserSchema), UserController.create);
+
+routes.post("/auth", AuthenticationController.authenticate);
 
 routes.get("/health", (req, res) => {
     return res.send({message: "connected with success"})
