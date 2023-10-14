ed4680798651a777377a88b98266ca5d62b20039
codeasrof <vitalidade.08-devorador@icloud.com>
2023-10-14 10:32:27 -0300

    Post route and controller

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,20 @@
-const Router = require("express");
-const schemaValidator = require("../apps/middlewares/schemaValidator");
-const UserController = require("../apps/controllers/UserController");
-const AuthenticationController = require("../apps/controllers/AuthenticationController");
-const AuthenticationMiddleware =  require("../apps/middlewares/authentication");
-const UserSchema = require("../schema/create.user.schema.json");
-const AuthSchema = require("../schema/auth.schema.json");
-const {upload} = require("../configs/multer");
-const FileController = require("../apps/controllers/FileController");
+const Router = require("express")
+const schemaValidator = require("../apps/middlewares/schemaValidator")
+const UserController = require("../apps/controllers/UserController")
+const PostController = require("../apps/controllers/PostController")
+const AuthenticationController = require("../apps/controllers/AuthenticationController")
+const AuthenticationMiddleware =  require("../apps/middlewares/authentication")
+const UserSchema = require("../schema/create.user.schema.json")
+const AuthSchema = require("../schema/auth.schema.json")
+const PostSchema = require("../schema/post.schema.json")
+const {upload} = require("../configs/multer")
+const FileController = require("../apps/controllers/FileController")
 const routes = Router();
 
 
-routes.post("/users", schemaValidator(UserSchema), UserController.create);
+routes.post("/users", schemaValidator(UserSchema), UserController.create)
 
-routes.post("/auth",schemaValidator(AuthSchema), AuthenticationController.authenticate);
+routes.post("/auth",schemaValidator(AuthSchema), AuthenticationController.authenticate)
 
 routes.use(AuthenticationMiddleware)
 
@@ -20,6 +22,7 @@ routes.put("/user", UserController.update)
 routes.delete("/user", UserController.delete)
 routes.get("/user-profile", UserController.userProfile)
 routes.post("/upload", upload.single("image"), FileController.upload)
+routes.post("/new-post", schemaValidator(PostSchema), PostController.create)
 
 routes.get("/health", (req, res) => {
     return res.send({message: "connected with success"})
